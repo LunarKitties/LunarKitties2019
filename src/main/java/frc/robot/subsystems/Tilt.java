602f8f4efb02ee0ab35cbd50f0c3225787c62a69
Lunar Kitties <lunarkitties5930@gmail.com>
2019-02-13 21:21:51 -0600

    Optimized Imports

--- src/main/java/frc/robot/subsystems/Tilt.java
+++ src/main/java/frc/robot/subsystems/Tilt.java
@@ -8,12 +8,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
-import frc.robot.commands.lift.OperateLift;
 import frc.robot.commands.tilt.OperateTilt;
 
 /**
